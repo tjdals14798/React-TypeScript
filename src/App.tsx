48b3968ee2af8291d9564b9a3bce0e25295c061a
성민 <tjdals14798@naver.com>
2023-01-18 17:34:30 +0900

    typescript 시작

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button, Nav } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  type Name = string | number;
+  let name1 :Name = "123";
+  let num : Name = 123;
+
+  return (
+    <div className="App">
+      <Nav>{name1} {typeof name1}</Nav>
+      <Nav>{num} {typeof num}</Nav>
+
+    </div>
+  );
+}
+
+export default App;
